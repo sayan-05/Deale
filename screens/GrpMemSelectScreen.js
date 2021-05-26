ccde81acd77dcb6846a64899bdeaec9f934f7122
Sayan Biswas <poorprogrammer-python@users.noreply.github.com>
2021-05-26 15:27:38 +0530

    individual element selection inside map function done

--- screens/GrpMemSelectScreen.js
+++ screens/GrpMemSelectScreen.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, ScrollView , TextInput } from 'react-native';
 import { useAtom } from "jotai"
 import { friendsListAtom } from "../atomState"
+import Icon from 'react-native-vector-icons/AntDesign'
+
 
 const GrpMemSelectScreen = () => {
     const [friendsList] = useAtom(friendsListAtom)
+    const [isPressedComponent, setIsPressedComponent] = useState([])
+    const [nameField,setNameField] = useState('')
+
 
     return (
         <View style={{
             flex: 1,
             backgroundColor: '#fff',
             width: '100%',
-            height: '90%'
+            height: 100,
+            justifyContent : 'center',
+            alignItems : 'center'
         }} >
-            <ScrollView  >
+            <Text>Group Name</Text>
+            <TextInput style={{
+                backgroundColor: 'rgb(242, 242, 242)',
+                width: '90%',
+                height: 45,
+                textAlign: 'center',
+                borderRadius: 5
+            }}
+            onChangeText = {v => setNameField(v)} />
+            <ScrollView style = {{
+                width : '100%'
+            }} >
                 {
                     friendsList.map(
                         (i) => {
                             return (
-                                <TouchableOpacity>
+                                <TouchableOpacity onPress={
+                                    () => {
+                                        setIsPressedComponent((prevState) => {
+                                            const prevStateCopy = [...prevState]
+                                            if (prevStateCopy.includes(i._id)) {
+                                                const index = prevStateCopy.indexOf(i._id)
+                                                prevStateCopy.splice(index, 1)
+                                            } else {
+                                                prevStateCopy.push(i._id)
+                                            }
+                                            return prevStateCopy
+                                        })
+                                    }
+                                    
+                                }
+                                style = {{
+                                    width : '100%',
+                                }} >
                                     <View
                                         key={i._id}
                                         style={{
@@ -26,8 +61,8 @@ const GrpMemSelectScreen = () => {
                                             flexDirection: 'row',
                                             marginVertical: 5,
                                             marginBottom: 10,
-                                            backgroundColor: 'grey',
-                                            height: 50
+                                            backgroundColor: isPressedComponent.includes(i._id) ? "blue" : "grey",
+                                            height: 50,
                                         }}
                                         key={i._id} >
                                         <Text style={{
@@ -40,6 +75,9 @@ const GrpMemSelectScreen = () => {
                     )
                 }
             </ScrollView>
+            <TouchableOpacity>
+            <Icon name="rightcircle" size={45} color="rgb(179, 252, 195)" />
+            </TouchableOpacity>
         </View >
     )
 }
